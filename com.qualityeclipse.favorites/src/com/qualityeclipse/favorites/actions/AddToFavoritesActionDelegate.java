020bebcd76a57af4a9f9bb8460be228ce27ab2fe
vista2001 <lytyphoon1010@163.com>
2014-01-23 15:27:12 +0800

    finished chp6 examples,this part containts only Action configuration

--- com.qualityeclipse.favorites/src/com/qualityeclipse/favorites/actions/AddToFavoritesActionDelegate.java
+++ com.qualityeclipse.favorites/src/com/qualityeclipse/favorites/actions/AddToFavoritesActionDelegate.java
@@ -3,6 +3,8 @@ package com.qualityeclipse.favorites.actions;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
@@ -10,7 +12,7 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-public class AddToFavoritesActionDelegate implements IObjectActionDelegate , IViewActionDelegate {
+public class AddToFavoritesActionDelegate implements IObjectActionDelegate , IViewActionDelegate , IEditorActionDelegate {
 
 	private IWorkbenchPart targetPart;
 
@@ -34,6 +36,11 @@ public class AddToFavoritesActionDelegate implements IObjectActionDelegate , IVi
 	@Override
 	public void init(IViewPart view) {
 		this.targetPart =  view;
+	}
+
+	@Override
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		this.targetPart = targetEditor;
 	}
 
 }
